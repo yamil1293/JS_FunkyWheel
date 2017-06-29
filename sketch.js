b3dbc2d0ea7ae98bb6b829f74834eaabb59e2914
Student <student@GAs-MacBook-Air-3.local>
2017-06-29 15:47:22 -0400

    reverting errors

--- sketch.js
+++ sketch.js
@@ -13,8 +13,8 @@ function preload() {
 
 function setup() {
     // Creates the canvas, sets a background color and the smooth out the following objects. 
-   // createCanvas(800, 800);
-   // background(255);
+    createCanvas(800, 800);
+    background(255);
     smooth();
 
     // Creates a button to pause/resume the above song and make it toggable.
